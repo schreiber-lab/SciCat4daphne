02436ea58759f56f61a14ec2b9c098d6f1bb184e
Linus Pithan <linus.pithan@gmail.com>
2022-04-07 10:35:08 +0200

    use offical image again

--- deploy/build_frontend_image.sh
+++
@@ -1,8 +0,0 @@
-cd frontend
-
-#cp -r src/assets/images  ../../scicatlive4daphne/config/catanie/images
-#cp -r src/assets/icons  ../../scicatlive4daphne/config/catanie/icons
-
-docker build -t pithan/frontend:latest .
-docker login --username=pithan
-docker push pithan/frontend:latest
