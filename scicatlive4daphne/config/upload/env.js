73a692222824f56f2cc9329f47f00dac36a24ddb
Linus Pithan <linus.pithan@gmail.com>
2022-05-15 21:40:27 +0200

    remove tuebingen server from default config

--- scicatlive4daphne/config/upload/env.js
+++ scicatlive4daphne/config/upload/env.js
@@ -1,5 +1,5 @@
 window.env = {
-  "REACT_APP_API_URL": "http://u-017-s197.v263.uni-tuebingen.de/api/v3",
-  "REACT_APP_API_URL2": "http://u-017-s197.v263.uni-tuebingen.de",
+  "REACT_APP_API_URL": "http://your-scicat-domain.org/api/v3",
+  "REACT_APP_API_URL2": "http://your-scicat-domain.org",
   "REACT_APP_STORE_KEY": "scicat"
 }
